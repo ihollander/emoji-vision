ffd856d7175b675b06887ed2d5a89fd7c31a999c
Ian Hollander <ianholla@gmail.com>
2024-11-17 06:31:57 -0500

    Add SSL plugin for dev on local network

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
+import basicSsl from "@vitejs/plugin-basic-ssl"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import { comlink } from "vite-plugin-comlink"
 
 export default defineConfig({
-  plugins: [react(), comlink()],
+  plugins: [basicSsl(), react(), comlink()],
   worker: {
     plugins: () => [comlink()],
   },
