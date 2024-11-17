c2e8df9565ad7df634af2b447d778f605dfdcd79
Ian Hollander <ianholla@gmail.com>
2024-11-17 09:27:14 -0500

    Add vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,17 @@ import basicSsl from "@vitejs/plugin-basic-ssl"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import { comlink } from "vite-plugin-comlink"
+import { VitePWA } from "vite-plugin-pwa"
 
 export default defineConfig({
-  plugins: [basicSsl(), react(), comlink()],
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+    }),
+    basicSsl(),
+    react(),
+    comlink(),
+  ],
   worker: {
     plugins: () => [comlink()],
   },
